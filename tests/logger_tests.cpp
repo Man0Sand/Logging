9c56713ab7adb68efd565915523cf60cf65dab20
Man0Sand <veli-matti.sainio@hotmail.com>
2018-09-23 23:32:51 +0300

    Implemented unit tests of Logger.
    - Replaced the English implementation with C++.

--- tests/logger_tests.cpp
+++ tests/logger_tests.cpp
@@ -5,45 +5,99 @@
 #include <gtest/gtest.h>
 
 #include "log_mock.h"
+#include "logger.h"
 
 TEST(LoggerTest, TestDifferentLogs)
 {
-    // construct log mocks
-    // construct logger
-    // execute logger writing with proper level and ensure log mocks are untouched
-    // set mock log 1, write to logger, ensure that done
-    // set mock log 2, write to logger, ensure that done
-    // set nullptr log, write to logger (should pass if nullptr checking works)
+    LogMock log_mock_1 = LogMock();
+    LogMock log_mock_2 = LogMock();
+    Logger logger = Logger();
+
+    logger.write("", Logger::kCritical);
+    ASSERT_EQ(0, log_mock_1.get_call_count());
+    ASSERT_EQ(0, log_mock_2.get_call_count());
+
+    logger.set_logger(&log_mock_1);
+    ASSERT_EQ(1, log_mock_1.get_call_count());
+    ASSERT_EQ(0, log_mock_2.get_call_count());
+
+    logger.set_logger(&log_mock_2);
+    ASSERT_EQ(1, log_mock_1.get_call_count());
+    ASSERT_EQ(1, log_mock_2.get_call_count());
+
+    logger.set_logger(nullptr);
+    ASSERT_EQ(1, log_mock_1.get_call_count());
+    ASSERT_EQ(1, log_mock_2.get_call_count());
 }
 
 TEST(LoggerTest, TestCriticalLogLevel)
 {
-    // construct a log mock
-    // construct logger
-    // set mock log
-    // write to logger with highest, medium and lowest log level
-    // ensure correct behavior after each call
+    LogMock log_mock = LogMock();
+    Logger logger = Logger();
+    logger.set_logger(&log_mock);
+    logger.set_level(Logger::kCritical);
+
+    logger.write("", Logger::kCritical);
+    ASSERT_EQ(1, log_mock.get_call_count());
+
+    logger.write("", Logger::kWarning);
+    ASSERT_EQ(1, log_mock.get_call_count());
+
+    logger.write("", Logger::kDebug);
+    ASSERT_EQ(1, log_mock.get_call_count());
 }
 
 TEST(LoggerTest, TestWarningLogLevel)
 {
-    // construct a log mock
-    // construct logger
-    // set mock log
-    // write to logger with highest, medium and lowest log level
-    // ensure correct behavior after each call
+    LogMock log_mock = LogMock();
+    Logger logger = Logger();
+    logger.set_logger(&log_mock);
+    logger.set_level(Logger::kWarning);
+
+    logger.write("", Logger::kCritical);
+    ASSERT_EQ(1, log_mock.get_call_count());
+
+    logger.write("", Logger::kWarning);
+    ASSERT_EQ(2, log_mock.get_call_count());
+
+    logger.write("", Logger::kDebug);
+    ASSERT_EQ(2, log_mock.get_call_count());
 }
 
 TEST(LoggerTest, TestDebugLogLevel)
 {
-    // construct a log mock
-    // construct logger
-    // set mock log
-    // write to logger with highest, medium and lowest log level
-    // ensure correct behavior after each call
+    LogMock log_mock = LogMock();
+    Logger logger = Logger();
+    logger.set_logger(&log_mock);
+    logger.set_level(Logger::kDebug);
+
+    logger.write("", Logger::kCritical);
+    ASSERT_EQ(1, log_mock.get_call_count());
+
+    logger.write("", Logger::kWarning);
+    ASSERT_EQ(2, log_mock.get_call_count());
+
+    logger.write("", Logger::kDebug);
+    ASSERT_EQ(3, log_mock.get_call_count());
 }
 
 TEST(LoggerTest, TestGettersAndSetters)
 {
-    // Test getter and setter for each log level
+    Logger logger = Logger();
+    ASSERT_EQ(Logger::kCritical, logger.get_level());
+
+    logger.set_level(Logger::kDebug);
+    ASSERT_EQ(Logger::kDebug, logger.get_level());
+
+    logger.set_level(Logger::kWarning);
+    ASSERT_EQ(Logger::kWarning, logger.get_level());
+
+    logger.set_level(Logger::kError);
+    ASSERT_EQ(Logger::kError, logger.get_level());
+
+    logger.set_level(Logger::kInfo);
+    ASSERT_EQ(Logger::kInfo, logger.get_level());
+
+    logger.set_level(Logger::kCritical);
+    ASSERT_EQ(Logger::kCritical, logger.get_level());
 }
